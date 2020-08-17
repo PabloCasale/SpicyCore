d7da295628a4ea6c1de84795977ece367d112200
Pablo <pablocasale83@gmail.com>
2020-08-17 14:19:38 -0300

    Added coupons views and controller methods

---
+++ SpicyCore/Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpicyCore.Data;
+using SpicyCore.Models;
+
+namespace SpicyCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CouponController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CouponController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Coupons.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupon coupons)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    coupons.Picture = p1;
+                }
+                _db.Coupons.Add(coupons);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coupons);
+
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var coupons = await _db.Coupons.SingleOrDefaultAsync(x => x.Id == id);
+            if (coupons == null)
+            {
+                return NotFound();
+            }
+            return View(coupons);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Coupon coupons)
+        {
+            if (coupons.Id == 0)
+            {
+                return NotFound();
+            }
+
+            var couponFromDb = await _db.Coupons.Where(c => c.Id == coupons.Id).FirstOrDefaultAsync();
+
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    couponFromDb.Picture = p1;
+                }
+                couponFromDb.MinmumAmount = coupons.MinmumAmount;
+                couponFromDb.Name = coupons.Name;
+                couponFromDb.Discount = coupons.Discount;
+                couponFromDb.CouponType = coupons.CouponType;
+                couponFromDb.IsActive = coupons.IsActive;
+
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coupons);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var coupon = await _db.Coupons.SingleOrDefaultAsync(x => x.Id == id);
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+
+            return View(coupon);
+        }
+
+        //GET - DELETE
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await _db.Coupons.FindAsync(id);
+
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+            
+            return View(coupon);
+
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ConfirmDelete(int id)
+        {
+            var coupon = await _db.Coupons.SingleOrDefaultAsync(x => x.Id == id);
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+
+            _db.Coupons.Remove(coupon);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
---
+++ SpicyCore/Data/Migrations/20200816235609_AddCouponsToDatabase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpicyCore.Data.Migrations
+{
+    public partial class AddCouponsToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    CouponType = table.Column<string>(nullable: false),
+                    Discount = table.Column<double>(nullable: false),
+                    MinmumAmount = table.Column<double>(nullable: false),
+                    Picture = table.Column<byte[]>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
